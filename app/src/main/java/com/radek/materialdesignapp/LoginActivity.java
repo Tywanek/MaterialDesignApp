644f301b512f943019ed0e5930e1e695bb3cb740
Radek <developer@mypos.pl>
2017-05-20 11:54:14 +0200

    drawer added

--- app/src/main/java/com/radek/materialdesignapp/LoginActivity.java
+++ app/src/main/java/com/radek/materialdesignapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.radek.materialdesignapp;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,10 +11,16 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Radek on 19.05.2017.
@@ -21,11 +28,20 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     private static String mypassword = "ala";
+    @InjectView(R.id.android)
+    ImageView android;
+    @InjectView(R.id.login)
+    EditText login;
+    @InjectView(R.id.password)
+    EditText password;
+    @InjectView(R.id.password_imput_layout)
+    TextInputLayout passwordImputLayout;
+    @InjectView(R.id.fab)
+    FloatingActionButton fab;
+    @InjectView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
 
-    private EditText passwordEditText;
-    private FloatingActionButton fab;
-    private TextInputLayout passwordInputLayout;
-    private  CoordinatorLayout coordinatorLayout;
+
     private Animation shake;
 
     private View.OnClickListener myFabListener = new View.OnClickListener() {
@@ -34,13 +50,6 @@ public class LoginActivity extends AppCompatActivity {
             Snackbar.make(coordinatorLayout, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
             startActivity(new Intent(getApplicationContext(), DetailActivity.class));
-//            } else {
-//            if (passwordEditText.getText().toString().equals(mypassword)) {
-//                startActivity(new Intent(getApplicationContext(), DetailActivity.class));
-//            } else {
-//             ///   passwordEditText.setError("to nie to hasło");
-//                passwordInputLayout.setError("to nie to haslo");
-//            }
         }
     };
 
@@ -48,24 +57,38 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_layout);
-        initView();
+        ButterKnife.inject(this);
+
         setView();
         initToolbar();
 
         shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+       // android.startAnimation(shake);
+}
 
-    }
-
-    private void initView() {
-        passwordEditText = (EditText) findViewById(R.id.password);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        passwordInputLayout = (TextInputLayout) findViewById(R.id.password_imput_layout);
-        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
-
-    }
 
     private void setView() {
         fab.setOnClickListener(myFabListener);
+//        android.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                android.clearAnimation();
+//            }
+//        });
+
+        android.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                    int finalRadius = Math.max(android.getWidth(), android.getHeight()) / 2;
+                    Animator anim =
+                            ViewAnimationUtils.createCircularReveal(android, (int) event.getX(), (int) event.getY(), 0, finalRadius);
+                    android.setVisibility(View.VISIBLE);
+                    anim.start();
+                    return false;
+            }
+        });
+
+
 
     }
 
